34d35251287ab6a7b693e7fe6747db73405799bb
Spaguetto <48161646+Spaguetto@users.noreply.github.com>
2024-01-29 18:18:50 -0500

    Prueba estos cambios!

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Hola profesor")
+
+
+//hey! es un bonito dia para hacer tu primer Hola mundo en JS!
+//Escribe  un alert como el del ejemplo>
+
+console.log("Ten un lindo dia!");
+alert('y que sea muy especial!');
